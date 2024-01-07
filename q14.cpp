4fd1c550ec6af3bb8fa981ac95691fd9e73b98e3
Md-Quashar <152798122+Md-Quashar@users.noreply.github.com>
2024-01-07 17:47:58 +0530

    q14.cpp

--- q14.cpp
+++ q14.cpp
@@ -4,24 +4,29 @@ class swapp{
     int x;
     int y;
     public:
-        friend void _swap(int n1,int n2);
+        friend void _swap(swapp &o);
+        void set(){
+            cout<<"enter two numbers"<<endl;
+            cin>>x>>y;
+        }
+        void show(){
+            cout<<"after swapping"<<endl;
+            cout<<x<<endl;
+            cout<<y<<endl;
+        }
 };
- void _swap(int &n1,int &n2){
-     n1=n1^n2;
-     n2=n1^n2;
-     n1=n1^n2;
+ void _swap(swapp &o){
+     o.x=o.x^o.y;
+     o.y=o.x^o.y;
+     o.x=o.x^o.y;
     
  }
 int main()
 {
     swapp obj;
-    cout<<"enter two numbers"<<endl;
-    int x;
-    int y;
-    cin>>x>>y;
-   _swap(x,y);
-   cout<<x<<endl;
-   cout<<y<<endl;
+     obj.set();
+   _swap(obj);
+   obj.show();
 
    return 0;
 }
