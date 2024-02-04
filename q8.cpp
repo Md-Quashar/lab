10e3d1741f597f3fb04800897439f70a5949ee22
Md-Quashar <152798122+Md-Quashar@users.noreply.github.com>
2024-02-04 20:38:05 +0530

    q8.cpp

--- q8.cpp
+++ q8.cpp
@@ -1,35 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 class demo{
+    protected:
     int data;
     public:
      demo(){};
-      demo(int a){
-        cout<<"default constructor is called"<<endl;
-        data=a;
-      }
-       demo(demo &obj){
-         cout<<"copy constructor"<<endl;
-          data=obj.data;
-       }
        virtual ~demo(){
-            cout<<" base class virtual destructor will never called because of virtual"<<endl;
+            cout<<" base class virtual destructor will called after the child class destructor because of virtual"<<endl;
        }
 };
 class child: public demo{
      public:
+      child(int a){
+        cout<<"default constructor is called"<<endl;
+        data=a;
+      }
+      child(child &obj){
+         cout<<"copy constructor"<<endl;
+          data=obj.data;
+       }
     
     ~child(){
-        cout<<"Because of virtual destructor in base class child class destructor called"<<endl;
+        cout<<"Because of virtual destructor in base class child class destructor called first"<<endl;
     }
 };
 int main()
 {     
-    demo a(7);
-    demo b(a);
-     child o1;
+    demo a;
+    child o1(7);
+    child o2(o1);
      demo *ptr;
      ptr=&o1;
+     delete ptr;
     
    return 0;
 }
