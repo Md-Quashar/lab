79b2945bff082788b2ae1cfa093b7a5e4dcb8838
Md-Quashar <152798122+Md-Quashar@users.noreply.github.com>
2024-02-04 20:25:07 +0530

    q6.cpp

--- q6.cpp
+++ q6.cpp
@@ -8,8 +8,25 @@ class year{
         cin>>y;
      }
      ~year(){
-        if(y%4==0)cout<<"its leap year"<<endl;
-        else cout<<"not a leap year"<<endl;
+        // leap year if perfectly divisible by 400
+  if (y % 400 == 0) {
+    cout << y << " is a leap year.";
+  }
+  // not a leap year if divisible by 100
+  // but not divisible by 400
+  else if (y % 100 == 0) {
+    cout << y << " is not a leap year.";
+  }
+  // leap year if not divisible by 100
+  // but divisible by 4
+  else if (y % 4 == 0) {
+    cout << y << " is a leap year.";
+  }
+  // all other years are not leap years
+  else {
+    cout << y << " is not a leap year.";
+  }
+
      }
 };
 int main()
